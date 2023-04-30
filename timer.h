ed8fc2f594113e4e70378be5da9d5ee379715ffc
bobjoe <rowedanielp@gmail.com>
2023-04-30 20:57:35 +0000

    simple setup

--- timer.h
+++ timer.h
@@ -1,14 +1,30 @@
 /**
  * @author Jeff Butler 
+ * @author Daniel Neshyba-Rowe
  * 
 */
-#include "timer.c"
+
+#include <sel4cp.h>
+
+/**
+ * Types for timer driver
+*/
+typedef uint64_t timestamp_t;
+/* typedef unsigned long long uint64_t; */
+typedef unsigned int uint32_t;
+typedef void (*timer_callback_t)(int);
+
+
 /**
  * Initialises the driver. Initialises the timer with the registers 
  * mapped at the provided virtual address. This should start the 
  * internal counter and allow timers to be registered.
 */
-int start_timer(unsigned char *timer_vaddr);
+
+// NOTE: changed from:
+/* int start_timer(unsigned char *timer_vaddr); */
+// to:
+int start_timer(uintptr_t timer_vaddr);
 
 /**
  * Read the current time in microseconds from the internal counter timer (timer E).
@@ -23,7 +39,8 @@ timestamp_t get_time(void);
  * otherwise a unique identifier for this timeout. This identifier 
  * can be used to remove a timeout. After a timeout has occurred, 
  * or the timeout has been removed, the identifier may be reused.
-*/uint32_t register_timer(uint64_t delay, timer_callback_t callback, void *data);
+*/
+uint32_t register_timer(uint64_t delay, timer_callback_t callback, void *data);
 
 /**
  * Remove a previously registered timer callback, using the unique 
@@ -35,6 +52,10 @@ int remove_timer(uint32_t id);
  * time requests.
 */
 int stop_timer(void);
+
+// Currently thinking of replacing this with the notified() method below.
+// This is the way which the Australians do it, but the notified (taken
+// from example timer for odroid) definitely works
 /**
  * Function to be called by the IRQ dispatch whenever an IRQ is 
  * triggered by the timer hardware. The IRQ dispatch will pass 
@@ -42,4 +63,15 @@ int stop_timer(void);
  * with and an IRQ handler capability that must be used to 
  * acknowledge the IRQ.
 */
-int timer_irq(void *data, seL4_Word irq, seL4_IRQHandler irq_handler);
+/* int timer_irq(void *data, seL4_Word irq, seL4_IRQHandler irq_handler); */
+
+
+/**
+ * seL4 method of initializing the driver
+ */
+void init(void);
+
+/**
+ * seL4 method of recieving interrupts
+ */
+void notified(sel4cp_channel ch);
